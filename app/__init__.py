ac23aceaace8336e4e907b906077f9131df97f0c
root <root@DESKTOP-0R3QSFP.localdomain>
2018-08-18 16:59:23 +0200

    Added database models

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,71 @@
+import logging, os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
+# Create application instance
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from app import routes
+# Create database instance
+db = SQLAlchemy(app)
+
+# Create database migration instance
+migrate = Migrate(app, db)
+
+# Create login manager instance
+login = LoginManager(app)
+login.login_view = "login"
+
+# Flask Bootstrap instance
+bootstrap = Bootstrap(app)
+
+# Flask Mail instance
+mail = Mail(app)
+
+# Avoid circular dependancies
+from app import routes, models, errors
+
+# Logging Errors
+if not app.debug:
+    # Email Based Logging
+    if app.config["MAIL_SERVER"]:
+        auth = None
+
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+
+        secure = None
+
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost = (app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr = "no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs = app.config["ADMINS"], 
+            subject = "Microblog Failure",
+            credentials = auth,
+            secure = secure)
+        
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    
+    # File Based Logging
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    file_handler = RotatingFileHandler("logs/microblog.py", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    
+    app.logger.addHandler(file_handler)
+
+    # Create an info-based log
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
---
+++ app/models.py
@@ -0,0 +1,120 @@
+from app import db
+from app import login
+from datetime import datetime
+from werkzeug import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
+from time import time
+import jwt
+from app import app
+
+# Followers association table
+followers = db.Table("followers",
+    db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+    )
+
+# Users database table
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
+    comments = db.relationship("Comment", backref="author", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Many-to-many follow relationship
+    followed = db.relationship(
+        "User", 
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"), 
+        lazy="dynamic"
+    )
+
+
+    # Print an object the of class for debugging purposes
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+    # Hash the user password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    # Compare original password and hashed password
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # Return the URL for user avatar's image
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
+
+    # Follow another user
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    # Stop following a user
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    # Determine if user is following another user
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+    
+    # Query the db for posts of followed users, with user's own posts
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+    
+    # JWT Password reset methods
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode({"reset_password": self.id, "exp": time()+ expires_in}, app.config["SECRET_KEY"], algorithm='HS256').decode("utf-8")
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+    
+# Register function as user loader
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    image = db.Column(db.Text)
+    subtitle = db.Column(db.String(128))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    comments = db.relationship("Comment", backref="post", lazy="dynamic")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post {self.body}>"
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr(self):
+        return f"<Comment {self.body}>"
+
+
+
