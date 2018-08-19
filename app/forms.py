53e7a45845fae14dbe95dc0159993812fa96a734
root <root@DESKTOP-0R3QSFP.localdomain>
2018-08-19 15:12:47 +0200

    Added login and registration forms

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    remember_me = BooleanField("Remember me")
+    remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
 
 class RegistrationForm(FlaskForm):
@@ -13,4 +14,49 @@ class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
+
+    # Add custom validator to prevent duplicate usernames
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username.")
+    
+    # Add custom validator to prevent duplicate emails
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different email address.")
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=140)])
     submit = SubmitField("Submit")
+
+    # Overloaded constructor
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+    
+    # Validate usename in edit profile form
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError("Please use a different username")
+
+# Blog Submission form
+class PostForm(FlaskForm):
+    post = TextAreaField("Say something", validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField("Submit")
+
+# Reset password request form
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+# Password reset form
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Request Password Reset")
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,73 @@
-from flask import render_template, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
+from app.models import User, Post
+from app.email import send_password_reset_email
+from datetime import datetime
 
 # Timeline page
 @app.route("/")
 @app.route("/index")
 def index():
+    """ Display the user timeline"""
     return render_template("index.html")
 
 # Login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    # Check if user is logged in already
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    # Instantiate a login object from LoginForm class
     form = LoginForm()
+    
+    # Process user-submitted data from form
+    if form.validate_on_submit():
+        # Get user from database
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # Validate user input from form
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        
+        # Login the user and remember the user
+        login_user(user, remember=form.remember_me.data)
+
+        # Redirect after logging in
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+
+        return redirect(next_page)
+    # Render template
+    return render_template("login.html", title="Sign In", form=form)
 
     
-
 @app.route("/logout")
 def logout():
-    """Log the user out"""
+    logout_user()
+    return redirect(url_for("index"))
     
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register the user"""
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    
+    return render_template("register.html", title="Register", form=form)
 
 # Profile page
 @app.route("/user/<username>")
